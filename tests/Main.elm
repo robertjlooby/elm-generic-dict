bdbbada5b467af4d85951bd6e37d99c3eb79aa3b
Rob Looby <robertjlooby@gmail.com>
2018-02-08 21:49:04 -0600

    Update elm-test

--- tests/Main.elm
+++ tests/Main.elm
@@ -1,18 +1,13 @@
-port module Main exposing (..)
+module Main exposing (..)
 
-import Json.Encode exposing (Value)
-import Test exposing (describe)
-import Test.Runner.Node exposing (run)
 import GenericDictTest
 import GenericSetTest
+import Test exposing (..)
 
 
-main =
-    run emit <|
-        describe "robertjlooby/elm-generic-dict tests"
-            [ GenericDictTest.tests
-            , GenericSetTest.tests
-            ]
-
-
-port emit : ( String, Value ) -> Cmd msg
+suite : Test
+suite =
+    describe "robertjlooby/elm-generic-dict tests"
+        [ GenericDictTest.tests
+        , GenericSetTest.tests
+        ]
